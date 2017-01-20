72ac73ca80a96d87c16a58f5a44c4ca0eb98947a
Nicolas YING <songshengying@aol.com>
2017-01-20 16:40:16 +0800

    Toll Station Release

--- code/simulation.m
+++ code/simulation.m
@@ -4,5 +4,20 @@ L = 3; % Regular lane number
 cell_size = 0.5; % cutting the road into small cells of 0.25 m^2
 
 % decision period is 1s, thus minimum speed taken into account is 0.5 m/s
-toll_barrier_state = zeros(B,50); % track vehicle departing from the tollbooth with historical info
+toll_barrier_state = zeros(70,B); % track vehicle departing from the tollbooth with historical info
+toll_barrier_config = [3,3,3,3,3,3,3,3; 20,20,20,20,20,20,20,20]; 
+% line 1 for vehicle types: 1, small, 2, medium, 3, large
+% line 2 for delay caused by charge mechanisms: 20, conventional, 10, exact exchange, 2,
+% electronic
+flow_total = 300; % total flow
+flow_instant = zeros(901,1); % number of vehicles per 15 minutes
+% distribute flow into each second
+for i=1:flow_total
+    ind = floor(rand()*900);
+    flow_instant(ind) = flow_instant(ind) + 1;
+end
 
+for i=1:900 % one simulation per second;
+[toll_barrier_state, flow_queue] = updateTollStation(flow_total, flow_instant(i), toll_barrier_state, toll_barrier_config);
+flow_instant(i+1) = flow_queue + flow_instant(i+1);
+end
