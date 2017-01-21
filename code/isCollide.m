45783e0a0c41cf0ce5fde08213a15c5d73a5ad70
GaspardQin <gaspard.qin@gmail.com>
2017-01-21 13:48:14 +0800

    isCollide bug fixed
    the def of angle changed

--- code/isCollide.m
+++ code/isCollide.m
@@ -2,10 +2,10 @@ function[collide] = isCollide(pos1,pos2,type1,type2)
 % pos including [x;y;alpha] alpha is between 0 to pi (pi/2 is vertical)
 global width_veh 
 global length_veh 
-%width_veh = [1, 3*sqrt(2)/2]; only use for test
-%length_veh = [3,3*sqrt(2)];
-%type1 = 1; type2 = 2;
-%pos1 = [4.5;1.5;0]; pos2=[5.75;3.75;pi/4];
+width_veh = [1, 3*sqrt(2)/2]; %only use for test
+length_veh = [3,3*sqrt(2)];
+type1 = 1; type2 = 2;
+pos1 = [4.5;1.5;pi/2]; pos2=[4.75;4.75;pi/4];
 
 dist = norm(pos2(1:2,:)-pos1(1:2,:));
 y2 = (pos2(1)-pos1(1))*sin(pos1(3)) + (pos2(2)-pos1(2))*cos(pos1(3));
@@ -18,8 +18,8 @@ elseif abs(pos2(3)-pos1(3)) < 0.002
     notcollide4 = dist > (width_veh(type1)+width_veh(type2))/2;
     collide = ~notcollide4;
 else
-    notcollide1 = (abs((y2 - width_veh(type1)/2 - width_veh(type2)/2/cos(pos2(3)-pos1(3)))/tan(pos2(3)-pos1(3))) - (length_veh(type1)/2-x2) > 0.002);
-    notcollide2 = (abs((y2 - width_veh(type1)/2 - width_veh(type2)/2/cos(pos2(3)-pos1(3)))/sin(pos2(3)-pos1(3))) -( length_veh(type2)/2 - width_veh(type2)/2*tan(pos2(3)-pos1(3))) > 0.002);
+    notcollide1 = (abs((abs(x2) - width_veh(type1)/2 - width_veh(type2)/2/cos(pos2(3)-pos1(3)))/tan(pos2(3)-pos1(3))) - (length_veh(type1)/2-y2) > 0.002);
+    notcollide2 = (abs((abs(x2) - width_veh(type1)/2 - width_veh(type2)/2/cos(pos2(3)-pos1(3)))/sin(pos2(3)-pos1(3))) -( length_veh(type2)/2 - width_veh(type2)/2*tan(pos2(3)-pos1(3))) > 0.002);
 
     collide = ~(notcollide1||notcollide2);
 end
