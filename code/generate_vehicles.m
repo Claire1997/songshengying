db24f5637eae15232dfe0abaeae7597748bc2081
GaspardQin <gaspard.qin@gmail.com>
2017-01-23 17:19:10 +0800

    600

--- code/generate_vehicles.m
+++ code/generate_vehicles.m
@@ -41,7 +41,7 @@ initial_speed = 5;
 % line 1 for vehicle types: 1, small, 2, medium, 3, large
 % line 2 for delay caused by charge mechanisms: 10, conventional, 5, exact exchange, 2,
 % electronic
-flow_total = 150; % total flow
+flow_total = 600; % total flow
 flow_instant = zeros(901,1); % number of vehicles per 15 minutes
 % distribute flow into each second
 for i=1:flow_total
